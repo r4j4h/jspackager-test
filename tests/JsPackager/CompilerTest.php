5f9c87cee4a5c17313dd9ae2abf0f4edf51e6f0f
Jasmine Hegman <jasmine@webpt.com>
2015-03-20 11:59:37 -0700

    Correcting test

--- tests/JsPackager/CompilerTest.php
+++ tests/JsPackager/CompilerTest.php
@@ -699,7 +699,7 @@ MANIFEST;
         $result = $compiler->compileDependencySet( $dependencySet );
 
         $compiledFilesContents = <<<COMPILEFILE
-window.remotescript_local_on_remote=!0;window.remotescript_remote_on_remote=!0;window.remotescript_script=!0;window.main_js=!0;
+window.main_local_file_before=!0;window.remotescript_local_on_remote=!0;window.remotescript_remote_on_remote=!0;window.remotescript_script=!0;window.main_local_file_after=!0;window.main_js=!0;
 
 COMPILEFILE;
 
