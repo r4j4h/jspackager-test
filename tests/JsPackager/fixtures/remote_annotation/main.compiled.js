547dd77855ece837216b499d9f00d351b469c7c7
Jasmine Hegman <jasmine@webpt.com>
2015-03-20 16:56:55 -0700

    Compiling the complex test for testing manifest resolver

---
+++ tests/JsPackager/fixtures/remote_annotation/main.compiled.js
@@ -0,0 +1,1 @@
+window.main_local_file_before=!0;window.remotescript_local_on_remote=!0;window.remotescript_remote_on_remote=!0;window.remotescript_script=!0;window.main_local_file_after=!0;window.main_js=!0;
