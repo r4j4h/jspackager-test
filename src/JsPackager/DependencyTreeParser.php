64c6afa93ead82498d71e954e62acba74f84f2d9
Jasmine Hegman <jasmine@webpt.com>
2015-06-08 13:30:12 -0700

    Updating dependencies in composer.lock Creating new FileResolverInterface Factoring AnnotationBasedFileResolver out of DependencyTreeParser and accompanying tests

--- src/JsPackager/DependencyTreeParser.php
+++ src/JsPackager/DependencyTreeParser.php
@@ -32,38 +32,14 @@ use JsPackager\Exception;
 use JsPackager\Exception\Parsing as ParsingException;
 use JsPackager\Exception\MissingFile as MissingFileException;
 use JsPackager\Exception\Recursion as RecursionException;
+use JsPackager\Resolver\AnnotationBasedFileResolver;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
 
 class DependencyTreeParser
 {
-    /**
-     * Regexp pattern for detecting annotations with optional space delimited arguments
-     * @var string
-     */
-    const TOKEN_PATTERN                 = '/@(.*?)(?:\s(.*))*$/i';
 
-    /**
-     * Regexp patterns for detecting file types via filename
-     * @var string
-     */
-    const SCRIPT_EXTENSION_PATTERN     = '/.js$/';
-    const STYLESHEET_EXTENSION_PATTERN = '/.css$/';
 
-    /**
-     * Definition list of allowed annotation tokens
-     * @var array
-     */
-    public $acceptedTokens = array(
-        'require',
-        'requireRemote',
-        'requireStyle',
-        'requireRemoteStyle',
-        'root',
-        'nocompile',
-        'tests',
-        'testsRemote'
-    );
 
     public $remoteFolderPath = 'shared';
 
@@ -106,6 +82,13 @@ class DependencyTreeParser
      */
     public $logger;
 
+
+    /**
+     * @var FileResolverInterface
+     */
+    private $resolver;
+
+
     public function __construct()
     {
         $this->logger = new NullLogger();
@@ -139,6 +122,35 @@ class DependencyTreeParser
     protected function getBasePathFromSourceFileWithoutTrailingSlash($sourceFilePath) {
         return ltrim( ( substr( $sourceFilePath, 0, strrpos($sourceFilePath, '/' ) ) ), '/' );
     }
+
+    /**
+     * @return mixed
+     */
+    public function createDefaultResolver()
+    {
+        $resolver = new AnnotationBasedFileResolver();
+        $resolver->logger = $this->logger;
+        $resolver->setFileHandler( $this->getFileHandler() );
+        $this->setResolver( $resolver );
+        return $resolver;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getResolver()
+    {
+        return ( $this->resolver ? $this->resolver : $this->createDefaultResolver() );
+    }
+
+    /**
+     * @param mixed $resolver
+     */
+    public function setResolver($resolver)
+    {
+        $this->resolver = $resolver;
+    }
+
     /**
      * Get the base path to a file.
      *
@@ -205,6 +217,8 @@ class DependencyTreeParser
      */
     public function parseFile( $filePath, $testsSourcePath = null, $recursing = false )
     {
+        $resolver = $this->getResolver();
+
         $this->logger->info("Parsing file '" . $filePath . "'.");
 
         // If we're starting off (not recursing), Clear parsed filename caches
@@ -264,7 +278,7 @@ class DependencyTreeParser
         {
             $this->logger->debug("Reading annotations in {$filePath}");
             // Read annotations
-            $annotationsResponse = $this->getAnnotationsFromFile( $filePath );
+            $annotationsResponse = $resolver->resolveDependenciesForFile( $filePath );
 
             $annotations = $annotationsResponse['annotations'];
             $orderingMap = $annotationsResponse['orderingMap'];
@@ -674,128 +688,6 @@ class DependencyTreeParser
         return $relativePath;
     }
 
-    /**
-     * Extracts tokens from a given file.
-     *
-     * Tokens are composed of the token start identifier (@), the action name, and any parameters separated by space.
-     *
-     * A token's definition continues until the end of the line.
-     *
-     * @param $filePath string File's path
-     */
-    protected function getAnnotationsFromFile( $filePath )
-    {
-        $this->logger->debug("Getting annotations from file '{$filePath}'.");
-        $fileHandler = $this->getFileHandler();
-
-        // Set up empty containers for all accepted annotations
-        $annotations = array();
-        $orderingMap = array();
-
-        foreach( $this->acceptedTokens as $acceptedToken )
-        {
-            $annotations[ $acceptedToken ] = array();
-        }
-
-        if ( $fileHandler->is_file( $filePath ) )
-        {
-
-            // Parse file line by line
-            $fh = $fileHandler->fopen( $filePath, 'r' );
-            while ( ( $line = $fileHandler->fgets($fh) ) !== false )
-            {
-
-                // Parse for annotation tokens with any arguments space delimited
-                $matches = array();
-                preg_match( self::TOKEN_PATTERN, trim( $line ), $matches );
-                $matchCount = count($matches);
-
-                // Is it a valid token line?
-                if ( $matchCount > 1 ) {
-
-                    // Handle the token action
-                    $action = $matches[1];
-
-                    // Skip if its not a defined token
-                    if ( !in_array( $action, $this->acceptedTokens ) )
-                    {
-                        $this->logger->debug("Found {$action} but it was not in acceptedTokens array.");
-                        continue;
-                    }
-
-                    // If it has params, split them
-                    if ( $matchCount > 2 && $matches[2] !== "" )
-                    {
-                        // Trim the space delimited arguments string and then explode it on spaces
-                        $params = explode( ' ',  trim( $matches[2] ) );
-
-                        foreach( $params as $key => $param ) {
-                            // Any extraneous spaces between arguments will
-                            // get exploded as empty strings so skip them
-                            if ( $param === "" )
-                                continue;
-
-                            $this->logger->info("Found '{$action}' annotation with param '{$param}'.");
-
-                            // Add argument for this action
-                            $annotations[ $action ][] = $param;
-
-                            // Append to annotation ordering map
-                            $annotationIndex = count( $annotations[ $action ] ) - 1;
-                            $orderingMap[] = array(
-                                'action' => $action,
-                                'annotationIndex' => $annotationIndex
-                            );
-                        }
-
-                    }
-                    // Otherwise we only care about the action
-                    else
-                    {
-                        $this->logger->info("Found '{$action}' action annotation.");
-
-                        $annotations[ $action ] = true;
-
-                        // Append to annotation ordering map
-                        $orderingMap[] = array(
-                            'action' => $action,
-                            'annotationIndex' => 0
-                        );
-                    }
-
-                }
-
-            }
-            $fileHandler->fclose($fh);
-
-        }
-
-        $this->logger->debug("Done getting annotations from file '{$filePath}'.");
-
-        return array(
-            'annotations' => $annotations,
-            'orderingMap' => $orderingMap
-        );
-    }
-
-
-    /**
-     * Function for array_filter
-     * @param  string  $file Filename
-     * @return boolean       True if $file has .js extension
-     */
-    protected function isJavaScriptFile($file) {
-        return preg_match( self::SCRIPT_EXTENSION_PATTERN, $file );
-    }
-
-    /**
-     * Function for array_filter
-     * @param  string  $file Filename
-     * @return boolean       True if $file has .js extension
-     */
-    private function isStylesheetFile($file) {
-        return preg_match( self::STYLESHEET_EXTENSION_PATTERN, $file );
-    }
 
 
     protected $fileHandler;
